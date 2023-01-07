5f9c2546d4396e63b21e24f542959fcca3194a65
Timo Nam <dwbh7358@gmail.com>
2023-01-07 22:56:39 +0900

    feat: that is the cleanup function

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+  function byeFn() {
+    console.log("bye :(");
+  }
+
+  function hiFn() {
+    console.log("Hi :)");
+    return byeFn;
+  }
+
+  useEffect(hiFn, []);
+
+  return  <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  console.log("I run all the time");
-
-  useEffect(() => {
-    console.log("Call the API...");
-  }, []);
-
-  useEffect(() => {
-    if (keyword.length > 5) {
-      console.log("SEARCH FOR", keyword);
-    }
-  }, [keyword]); 
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
 
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
